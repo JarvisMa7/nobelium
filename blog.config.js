036926bb74f880e4a2e1fd74cab891ac13fe72c5
JarvisMa7 <jarvisma.7@gmail.com>
2021-05-13 20:56:53 +0800

    Enable sort by date

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2017, // if leave this empty, current year will be used.
   postsPerPage: 10,
-  sortByDate: false,
+  sortByDate: true,
   showAbout: true, // WIP
   showArchive: true, // WIP
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
